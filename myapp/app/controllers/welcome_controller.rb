4a7a26b09bbe886c1bb8efa606d6912ab82d5842
Brian Kaney <brian@vermonster.com>
2015-09-08 00:49:19 -0400

    Expose postgresql and redis information

--- myapp/app/controllers/welcome_controller.rb
+++ myapp/app/controllers/welcome_controller.rb
@@ -1,5 +1,15 @@
 class WelcomeController < ApplicationController
   def index
-    render text: 'Welcome aboard'
+    render text: "Welcome aboard. Redis uptime: #{redis.info['uptime_in_seconds']}s. Postgresql: #{postgresql.tables.join}"
+  end
+
+  private
+
+  def postgresql
+    ActiveRecord::Base.connection
+  end
+
+  def redis
+    @redis ||= Redis.new(host: 'redis', port: 6379)
   end
 end
